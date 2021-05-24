b159449c634c868a270136356136749223b5041b
NadiaLayraAziza <nadialayra15@gmail.com>
2021-05-24 19:06:18 +0700

    Insert 3 data awal pada tabel kategori melalui seeder

--- database/seeders/KategoriSeeder.php
+++ database/seeders/KategoriSeeder.php
@@ -13,6 +13,22 @@ class KategoriSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\Models\Kategori::insert([
+            [
+                'kode_kategori' => 'KT001',
+                'nama_kategori' => 'Elektronik',
+                'keterangan' => 'Semua barang di ruang multimedia',
+            ],
+            [
+                'kode_kategori' => 'KT002',
+                'nama_kategori' => 'Kendaraan',
+                'keterangan' => 'Isi BBM setelah pinjam',
+            ],
+            [
+                'kode_kategori' => 'KT003',
+                'nama_kategori' => 'Ruangan',
+                'keterangan' => 'Semua ruangan di sekolah',
+            ],
+        ]);
     }
 }
