de1306a2cebec4fb5be029557506aa5d1b5b1996
NadiaLayraAziza <nadialayra15@gmail.com>
2021-05-24 19:05:12 +0700

    Mengatur fillable pada model Kategori agar data tersimpan di database

--- app/Models/Kategori.php
+++ app/Models/Kategori.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Kategori extends Model
 {
     use HasFactory;
+    protected $table="kategori"; // Eloquent akan membuat model Kategori menyimpan record di tabel kategori
+    protected $primaryKey = 'id'; // Memanggil isi DB Dengan primarykey
+    /**
+     * The attributes that are mass assignable. *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'kode_kategori',
+        'nama_kategori',
+        'keterangan',
+    ];
 }
