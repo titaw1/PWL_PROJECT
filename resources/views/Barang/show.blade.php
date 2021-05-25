28b79593e761c0054a786d4baf7b02a74762b7b5
Tita Wijayanti <wijayantitita094@gmail.com>
2021-05-25 22:34:09 +0700

    Memperbaiki view show barang

--- resources/views/Barang/show.blade.php
+++ resources/views/Barang/show.blade.php
@@ -14,60 +14,49 @@
             </nav>
         </div>
         <div class="col-md-6 col-sm-12 text-right">
-            <i class="icon-copy dw dw-add-file-1 fa-3x" aria-hidden="true"></i>
+            <i class="icon-copy fa fa-info-circle fa-3x" aria-hidden="true"></i>
         </div>
     </div>
 </div>
-<!-- Default Basic Forms Start -->
-<div class="pd-20 card-box mb-30">
-	@if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-	<form>
-		<div class="form-group row">
-			<label for="kode_barang" class="col-sm-12 col-md-2 col-form-label text-white">Kode Barang</label>
-			<div class="col-sm-12 col-md-10">
-				<input class="form-control" type="text" name="kode_barang" id="kode_barang" value="{{ $barang->kode_barang }}" aria-describedby="kode_barang" placeholder="Disabled input" disabled="">
-			</div>
-		</div>
-        <div class="form-group row">
-			<label for="id_kategori" class="col-sm-12 col-md-2 col-form-label text-white">Kategori</label>
-            <div class="col-sm-12 col-md-10">
-                <select class="custom-select col-12" type="id_kategori" name="id_kategori" id="id_kategori">
-                    <option value="{{ $barang->kategori->nama_kategori }}">Choose Kategori</option>
-                    @foreach ($kategori as $kt)
-                        <option value="{{$kt->id}}">{{$kt->nama_kategori}}</option>
-                    @endforeach
-                </select>
+<div class="product-wrap">
+    <div class="product-detail-wrap mb-30">
+        <div class="row">
+            <div class="col-lg-6 col-md-12 col-sm-12">
+            <div class="pd-20 card-box height-100-p" style="padding-left: 85px">
+                    <img height="300" width="300" @if($barang->gambar) src="{{ asset('storage/'.$barang->gambar) }}" @endif />
             </div>
-		</div>
-		<div class="form-group row">
-			<label for="nama_barang" class="col-sm-12 col-md-2 col-form-label text-white">Nama Barang</label>
-			<div class="col-sm-12 col-md-10">
-				<input class="form-control" type="text" name="nama_barang" id="nama_barang" value="{{ $barang->nama_barang }}" aria-describedby="nama_barang" placeholder="Disabled input" disabled="">
-			</div>
-		</div>
-		<div class="form-group row">
-			<label for="jumlah_barang" class="col-sm-12 col-md-2 col-form-label text-white">Jumlah</label>
-			<div class="col-sm-12 col-md-10">
-				<input class="form-control" type="text" name="jumlah_barang" id="jumlah_barang" value="{{ $barang->jumlah_barang }}" aria-describedby="jumlah_barang" placeholder="Disabled input" disabled="">
-			</div>
-		</div>
-        <div class="form-group row">
-            <label for="gambar" class="col-sm-2 col-form-label text-white">Gambar</label>
-            <div class="col-sm-10">
-                <img class="product" width="200" height="200" @if($barang->gambar) src="{{ asset('storage/'.$barang->gambar) }}" @endif />
-                <input type="file" class="uploads form-control" style="margin-top: 20px;" name="gambar">
+            </div>
+            <div class="col-lg-6 col-md-12 col-sm-12">
+                <div class="product-detail-desc pd-20 card-box height-100-p">
+                    <form>
+                        <div class="form-group row" style="padding-left: 90px">
+                            <label for="kode_barang" class="col-sm-10 col-md-4 col-form-label text-white">Kode Barang</label>
+                            <div class="col-md-6 col-sm-12">
+                                <input class="form-control" type="text" name="kode_barang" id="kode_barang" value="{{ $barang->kode_barang }}" aria-describedby="kode_barang" placeholder="Disabled input" disabled="">
+                            </div>
+                        </div>
+                        <div class="form-group row" style="padding-left: 90px">
+                            <label for="id_kategori" class="col-sm-10 col-md-4 col-form-label text-white">Kategori</label>
+                            <div class="col-md-6 col-sm-12">
+                                <input class="form-control" type="text" name="id_kategori" id="id_kategori" value="{{ $barang->kategori->nama_kategori }}" aria-describedby="id_kategori" placeholder="Disabled input" disabled="">
+                            </div>
+                        </div>
+                        <div class="form-group row" style="padding-left: 90px">
+                            <label for="nama_barang" class="col-sm-10 col-md-4 col-form-label text-white">Nama Barang</label>
+                            <div class="col-md-6 col-sm-12">
+                                <input class="form-control" type="text" name="nama_barang" id="nama_barang" value="{{ $barang->nama_barang }}" aria-describedby="nama_barang" placeholder="Disabled input" disabled="">
+                            </div>
+                        </div>
+                        <div class="form-group row" style="padding-left: 90px">
+                            <label for="jumlah_barang" class="col-sm-10 col-md-4 col-form-label text-white">Jumlah</label>
+                            <div class="col-md-6 col-sm-12">
+                                <input class="form-control" type="text" name="jumlah_barang" id="jumlah_barang" value="{{ $barang->jumlah_barang }}" aria-describedby="jumlah_barang" placeholder="Disabled input" disabled="">
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-	</form>
+    </div>
 </div>
-<!-- Default Basic Forms End -->
 @endsection
