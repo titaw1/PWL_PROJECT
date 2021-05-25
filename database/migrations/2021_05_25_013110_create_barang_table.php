7fd6b9b05a6c4308149539a6852b9f3d290a08de
Tita Wijayanti <wijayantitita094@gmail.com>
2021-05-25 09:01:59 +0700

    Membuat tabel barang melalui migration

--- database/migrations/2021_05_25_013110_create_barang_table.php
+++ database/migrations/2021_05_25_013110_create_barang_table.php
@@ -14,7 +14,13 @@ class CreateBarangTable extends Migration
     public function up()
     {
         Schema::create('barang', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('kode_barang', 50)->nullable();
+            $table->string('nama_barang', 50)->nullable();
+            $table->string('gambar',50)->nullable();
+            $table->integer('jumlah_barang')->nullable();
+            $table->unsignedBigInteger('id_kategori')->nullable();
+            $table->foreign('id_kategori')->references('id')->on('kategori');
             $table->timestamps();
         });
     }
