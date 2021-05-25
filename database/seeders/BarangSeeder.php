4a5a0a096d35b295653fe60e41d9870e68267b13
Tita Wijayanti <wijayantitita094@gmail.com>
2021-05-25 09:10:24 +0700

    Mengisi fillable pada model Barang agar data tersimpan dalam database

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -13,6 +13,28 @@ class BarangSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\Models\Kategori::insert([
+            [
+                'kode_barang' => 'MB01',
+                'nama_barang' => 'Mobil',
+                'gambar' => '/img/mobil.png',
+                'jumlah_barang' => 2,
+                'id_kategori' => 2,
+            ],
+            [
+                'kode_barang' => 'KM01',
+                'nama_barang' => 'Komputer',
+                'gambar' => '/img/komputer.jpg',
+                'jumlah_barang' => 50,
+                'id_kategori' => 1,
+            ],
+            [
+                'kode_barang' => 'LK01',
+                'nama_barang' => 'Mobil',
+                'gambar' => '/img/labkimia.jpg',
+                'jumlah_barang' => 2,
+                'id_kategori' => 3,
+            ],
+        ]);
     }
 }
