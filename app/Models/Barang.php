2182803fe7309ca5bb1add4a5b2dd0d1a7f85321
Tita Wijayanti <wijayantitita094@gmail.com>
2021-06-07 20:07:32 +0700

    Menambahkan relasi pada model Barang

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Kategori;
+use App\Models\BarangKeluar;
 
 class Barang extends Model
 {
@@ -27,5 +28,9 @@ class Barang extends Model
     public function kategori()
     {
         return $this->belongsTo(Kategori::class, 'id_kategori');
+    }
+    public function BarangKeluar()
+    {
+        return $this->hasMany(BarangKeluar::class);
     }
 }
