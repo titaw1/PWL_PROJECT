275ae561c95040cd228839a61b130152697cdc7c
Tita Wijayanti <wijayantitita094@gmail.com>
2021-06-19 10:14:43 +0700

    Update dropdown kategori pada create dan edit menu barang

--- resources/views/Barang/create.blade.php
+++ resources/views/Barang/create.blade.php
@@ -39,15 +39,14 @@
 				<input class="form-control" type="text" name="kode_barang" id="kode_barang" aria-describedby="kode_barang" placeholder="">
 			</div>
 		</div>
-        <div class="form-group row">
+        <div class="form row">
 			<label for="id_kategori" class="col-sm-12 col-md-2 col-form-label text-white">Kategori</label>
             <div class="col-sm-12 col-md-10">
-                <select class="custom-select col-12" type="id_kategori" name="id_kategori" id="id_kategori">
-                    <option value="">Choose Kategori</option>
-                    @foreach ($kategori as $kt)
-                        <option value="{{$kt->id}}">{{$kt->nama_kategori}}</option>
-                    @endforeach
-                </select>
+                <div class="input-group">
+                    <input id="kategori_nama" type="text" class="form-control" readonly="" required>
+                    <input id="id_kategori" type="hidden" name="id_kategori" value="{{ ('id_kategori') }}" required readonly="">
+                    <button type="button" class="btn btn-info btn-secondary" data-toggle="modal" data-target="#myModal"><b>Cari Kategori </b><span class="fa fa-search"></span></button>
+                </div>
             </div>
 		</div>
 		<div class="form-group row">
@@ -84,4 +83,61 @@
 	</form>
 </div>
 <!-- Default Basic Forms End -->
+<!-- Modal -->
+<div class="modal fade bd-example-modal-lg" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" >
+    <div class="modal-dialog modal-lg" role="document" >
+        <div class="modal-content" style="background: #fff;">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Cari Kategori</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <table id="lookup" class="table table-bordered table-hover table-striped">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Kode</th>
+                            <th>Kategori</th>
+                            <th>Keterangan</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($kategori as $data)
+                        <tr class="pilih" data-id_kategori="<?php echo $data->id; ?>" data-kategori_nama="<?php echo $data->nama_kategori; ?>" >
+                            <td>{{$loop->iteration}}</td>
+                            <td>{{$data->kode_kategori}}</td>
+                            <td>{{$data->nama_kategori}}</td>
+                            <td>{{$data->keterangan}}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Modal -->
+<script>
+    $(document).ready(function(){
+      $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
+    });
+</script>
+<script type="text/javascript">
+    $(document).on('click', '.pilih', function (e) {
+        document.getElementById("kategori_nama").value = $(this).attr('data-kategori_nama');
+        document.getElementById("id_kategori").value = $(this).attr('data-id_kategori');
+        $('#myModal').modal('hide');
+    });
+
+    $(function () {
+        $("#lookup, #lookup2").dataTable();
+    });
+</script>
 @endsection
